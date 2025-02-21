bb4bd2c45ed5a67c70ebed0886cfc0665db2bab2
Tony Cleem <tonycleem@gmail.com>
2025-02-21 07:03:43 +0700

    etup Flat changelist page.

--- property/admin.py
+++ property/admin.py
@@ -7,6 +7,16 @@ from .models import Flat
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner')
     readonly_fields = ['created_at']
-
+    list_display = ('address', 'price', 'new_building', 'created_at', 'town')
+    list_editable = ['new_building']
 
 admin.site.register(Flat, FlatAdmin)
+
+
+# Поля для отображения
+#
+#     Адрес квартиры
+#     Цена квартиры
+#     Новостройка
+#     Год постройки
+#     Город
